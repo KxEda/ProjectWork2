0ce1d2218378617d3a1b56760e31537f0f46ab5c
KxEda <k.quadira@gmail.com>
2025-12-12 08:53:40 +0100

    v19

--- src/test/java/hu/masterfield/steps/DigitalBankTestSteps.java
+++ src/test/java/hu/masterfield/steps/DigitalBankTestSteps.java
@@ -22,6 +22,13 @@ import java.util.Map;
 public class DigitalBankTestSteps {
     private static WebDriver driver;
     private static WebDriverWait wait;
+    BasePage basePage = new BasePage(driver, wait);
+    BannerPage bannerPage = new BannerPage(driver, wait);
+    LoginPage loginPage = new LoginPage(driver, wait);
+    HomePage homePage = new HomePage(driver, wait);
+    ProfilePage profilePage = new ProfilePage(driver, wait);
+    NewSavingsPage newSavingsPage = new NewSavingsPage(driver, wait);
+    ViewSavingsPage viewSavingsPage = new ViewSavingsPage(driver, wait);
 
     @Before
     public static void setup() {
@@ -40,14 +47,12 @@ public class DigitalBankTestSteps {
 
     @Given("the user opens the DigitalBank webpage")
     public void openDigitalBankWebpage() {
-        BasePage basePage = new BasePage(driver, wait);
         driver.get(basePage.getBaseUrl());
         basePage.pageIsLoaded(basePage.getDigitalBankLogo());
     }
 
     @And("accepts cookies")
     public void acceptCookies() {
-        BannerPage bannerPage = new BannerPage(driver, wait);
         bannerPage.pageIsLoaded(bannerPage.getCookieBanner());
         bannerPage.acceptCookies();
         bannerPage.checkIfCookiesBannerStillVisible();
@@ -56,7 +61,6 @@ public class DigitalBankTestSteps {
 
     @When("logs in with valid username and password using datatable")
     public void loginWithValidCredentials(DataTable table) {
-        LoginPage loginPage = new LoginPage(driver, wait);
         loginPage.pageIsLoaded(loginPage.getSignInButton());
         Map<String, String> loginData = table.asMap(String.class, String.class);
         String username = loginData.get("username");
@@ -66,28 +70,24 @@ public class DigitalBankTestSteps {
 
     @Then("the user is logged in successfully and can see the Home page")
     public void theUserIsLoggedInSuccessfullyAndCanSeeTheHomePage() {
-        HomePage homePage = new HomePage(driver, wait);
         homePage.pageIsLoaded(homePage.getHomePageMenuLogo());
         homePage.checkIfUserIsLoggedIn();
     }
 
     @When("enters invalid {string} and {string} to login")
     public void entersInvalidUserNameAndPasswordToLogin(String username, String password) {
-        LoginPage loginPage = new LoginPage(driver, wait);
         loginPage.pageIsLoaded(loginPage.getSignInButton());
         loginPage.login(username, password);
     }
 
     @Then("the login is failed, error invalid credentials is shown")
     public void failedLogin() {
-        LoginPage failedLoginPage = new LoginPage(driver, wait);
-        failedLoginPage.checkAlertBox();
-        failedLoginPage.checkAlertErrorText();
+        loginPage.checkAlertBox();
+        loginPage.checkAlertErrorText();
     }
 
     @And("is on the MyProfile page")
     public void navigateToMyProfilePage() {
-        HomePage homePage = new HomePage(driver, wait);
         homePage.openMyProfile();
         ProfilePage profilePage = new ProfilePage(driver, wait);
         profilePage.checkProfilePage();
@@ -95,7 +95,6 @@ public class DigitalBankTestSteps {
 
     @When("updates the mobilephone number using datatable")
     public void updatesTheMobilePhoneNumber(DataTable table) {
-        ProfilePage profilePage = new ProfilePage(driver, wait);
         Map<String, String> updateNumber = table.asMap(String.class, String.class);
         String mobilePhoneNumber = updateNumber.get("mobilePhoneNumber");
         profilePage.updateMobilePhoneData(mobilePhoneNumber);
@@ -103,13 +102,11 @@ public class DigitalBankTestSteps {
 
     @Then("update of the profile data is successful, success: Profile Updated Successfully.")
     public void updateOfTheProfileDataIsSuccessfulSuccessProfileUpdatedSuccessfully() {
-        ProfilePage profilePage = new ProfilePage(driver, wait);
         profilePage.checkUpdatedMobileData();
     }
 
     @When("updates the titleField")
     public void updatesTheTitleField(DataTable table) {
-        ProfilePage profilePage = new ProfilePage(driver, wait);
         Map<String, String> updateNumber = table.asMap(String.class, String.class);
         String selectedOption = updateNumber.get("titleField");
         profilePage.updateTitleData(selectedOption);
@@ -117,13 +114,11 @@ public class DigitalBankTestSteps {
 
     @Then("successful title update, success: Profile Updated Successfully.")
     public void successfulTitleUpdateSuccessProfileUpdatedSuccessfully() {
-        ProfilePage profilePage = new ProfilePage(driver, wait);
         profilePage.checkUpdatedTtile();
     }
 
     @And("on the New Savings page")
     public void onTheNewSavingsPage() {
-        HomePage homePage = new HomePage(driver, wait);
         homePage.navigateToSavingsMenu();
         NewSavingsPage newSavingsPage = new NewSavingsPage(driver, wait);
         newSavingsPage.pageIsLoaded(newSavingsPage.getSavingsPageTite());
@@ -131,20 +126,17 @@ public class DigitalBankTestSteps {
 
     @When("new saving is started using {string}, {string}, {string}, {string}")
     public void createsANewSaving(String accountType, String ownership, String accountName, String initialDeposit) {
-        NewSavingsPage newSavingsPage = new NewSavingsPage(driver, wait);
         newSavingsPage.pageIsLoaded(newSavingsPage.getSavingsPageTite());
         newSavingsPage.startNewSaving(accountType, ownership, accountName, initialDeposit);
     }
 
     @Then("new saving is created successfully")
     public void newSavingIsCreatedSuccessfully() {
-        ViewSavingsPage viewSavingsPage = new ViewSavingsPage(driver, wait);
         viewSavingsPage.checkIfSavingWasSuccessful();
     }
 
     @Then("new saving is failed, error: The {string} entered does not meet the minimum amount...")
     public void newSavingIsFailedErrorTheInitialDepositEnteredDoesNotMeetTheMinimumAmount(String initialDeposit) {
-        NewSavingsPage newSavingsPage = new NewSavingsPage(driver, wait);
         newSavingsPage.checkErrorText(initialDeposit);
     }
 }
